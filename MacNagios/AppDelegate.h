77e9aae943c858f174fb2d6e0976ce7fcf6446eb
Brad Peabody <bradpeabody@gmail.com>
2014-04-04 18:20:49 -0700

    Initial Commit

---
+++ MacNagios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MacNagios
+//
+//  Created by Brad Peabody on 4/4/14.
+//  Copyright (c) 2014 BGP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MacNagios/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MacNagios
+//
+//  Created by Brad Peabody on 4/4/14.
+//  Copyright (c) 2014 BGP. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
